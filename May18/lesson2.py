cc610cfd5218c4704ed186cf0d3d60f799ff3631
Selatin23 <133867063+Selatin23@users.noreply.github.com>
2023-05-18 21:44:21 +0600

    Update lesson2.py

--- May18/lesson2.py
+++ May18/lesson2.py
@@ -1,1 +1,31 @@
-print("Hello Github")
+#1
+user_input = input("Введите число:")
+user_int = int(user_input)
+
+result = user_int ** 2
+print("Результат:", result)
+
+#2
+user_input1 = input("Введите число:")
+user_input2 = input("Введите число:")
+user_int1 = int(user_input1)
+user_int2 = int(user_input2)
+
+result = (user_int1 + user_int2) / 2
+print("Результат:", result)
+
+#3
+user_input = input("Длина стороны квадрата:")
+user_int = int(user_input)
+
+result = user_int * user_int
+print("Результат:", result)
+
+#4
+user_input = input("КМ:")
+user_int = int(user_input)
+
+result = user_int * ml
+print("Результат мили:", result)
+
+#5
