7897395fa59b49911d7aef208166ca1f66415536
Sultan <133867063+Selatin23@users.noreply.github.com>
2023-09-26 21:48:53 +0600

    Update

--- CW_18.09(NetChat)/controller.py
+++ CW_18.09(NetChat)/controller.py
@@ -1,8 +1,51 @@
 from PyQt6.QtCore import QObject, pyqtSignal
 from logger import log
 
-# тут будет контроллер
+# state, signal, transition
 class Controller(QObject):
+    _state = "INIT"
+    _transitions = (
+        {'from':"INIT",         'to': "LOGIN",          'by': "DB_READY"},
+        {'from':"LOGIN",        'to': "AUTH",           'by': "GUI_LOGIN"},
+        {'from':"AUTH",         'to': "MAIN_WINDOW",    'by': "DB_AUTH_OK"},
+        {'from':"AUTH",         'to': "LOGIN",          'by': "DB_AUTH_BAD"},
+
+        {'from':"MAIN_WIN",     'to': "ADD_FRIEND",     'by': "UR_HELLO"},
+        {'from':"ADD_FRIEND",   'to': "MAIN_WIN",       'by': "IMMEDIATELY"},
+
+        {'from':"MAIN_WIN",     'to': "CHECK_MSG",      'by': "UR_MESSAGE"},
+        {'from':"CHECK_MSG",    'to': "MAIN_WIN",       'by': "IMMEDIATELY"},
+
+        {'from':"MAIN_WIN",     'to': "SEND_MSG",       'by': "GUI_SEND"},
+        {'from':"GUI_SEND",     'to': "MAIN_WIN",       'by': "IMMEDIATELY"},
+
+        {'from':"MAIN_WIN",     'to': "CHANGING_CHAT",  'by': "GUI_CHAT_CHANGE"},
+        {'from':"CHANGING_CHAT",'to': "MAIN_WIN",       'by': "IMMEDIATELY"},
+
+    )
+
+    def process_state(self):
+        match self.state:
+            case "INIT":
+                pass
+            case "LOGIN":
+                pass
+            case "AUTH":
+                pass
+            case "MAIN_WIN":
+                pass
+            case "ADD_FRIEND":
+                pass
+            case "CHECK_MSG":
+                pass
+            case "CHANGING_CHAT":
+                pass
+            case _:
+                log.w("Unknown state!")
+
+    def process_signal(self, signal_name):
+        pass
+
     switchWindow = pyqtSignal(str, str)
     def login(self, username):
         if username:
